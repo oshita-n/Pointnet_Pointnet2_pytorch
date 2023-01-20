f4b5f0a68415249c60296395ef242c61b1a4f99c
oshita-n <oshita-n@whispon.com>
2023-01-20 18:00:25 +0900

    Add script to make pcd file in inferential format

---
+++ convert_test_data.py
@@ -0,0 +1,23 @@
+import sys
+import open3d as o3d
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import glob
+
+input_dir = "data/"
+file_name_list = glob.glob(input_dir + "*.pcd")
+for file_name in file_name_list:
+    pcd = o3d.io.read_point_cloud(file_name)
+    points = np.asanyarray(pcd.points)
+    colors = np.array(np.asanyarray(pcd.colors)*255, dtype=np.int64)
+
+    with open(input_dir + os.path.splitext(os.path.basename(file_name))[0] + ".txt", "a") as f:
+        for i in range(len(points)):
+            x = points[i][0]
+            y = points[i][1]
+            z = points[i][2]
+            r = colors[i][0]
+            g = colors[i][1]
+            b = colors[i][2]
+            f.write(f"{x} {y} {z} {r} {g} {b}\n")
